bc09baf784c9f01cf04fa72695e023c3fd89865f
Muhammad Najimov <114020956+AliNajimov@users.noreply.github.com>
2022-10-25 15:38:21 +0500

    Update PrimeNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        for (int i=2;i<=printToInclusive; i++){
+            int count=0;
+            for(int j=0;j<=i;j++){
+                count++;
+            }
+            if(count==2){
+                System.out.println(i);
+            }
+        }
     }
 }
