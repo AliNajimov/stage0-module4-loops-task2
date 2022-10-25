b67a25f6ab9e9f422f7051e206074083c96efc59
Muhammad Najimov <114020956+AliNajimov@users.noreply.github.com>
2022-10-26 01:19:54 +0500

    Update NaturalNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
@@ -4,7 +4,7 @@ public class NaturalNumbers {
     public void naturalNumbersPrinter(int lastPrinted) {
         int temp=0;
         while(temp<=lastPrinted){
-            Syste.out.println(temp);
+            System.out.println(temp);
             temp++;
         }
     }
