ad954bcbd10789c7a873dbe9f6cd3eff32051d3e
Muhammad Najimov <114020956+AliNajimov@users.noreply.github.com>
2022-10-26 01:09:42 +0500

    Update WordsBuilder.java

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
-
+        String str = "";
+        while ((int) chars>0){
+             str+=chars;
+        }
+        System.out.println(str);
+        
     }
 }
