01d80786ac824af11705e831329617af23f75ef8
Muhammad Najimov <114020956+AliNajimov@users.noreply.github.com>
2022-10-26 01:17:52 +0500

    Update FactorialNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-
+        int temp=0;
+        while(printToInclusive>0){
+            if(temp==0){
+                System.out.println(1);
+            }else {
+                temp*=temp;
+                System.out.println(temp);
+            }
+            temp++;
+            printToInclusive--;
+        }
     }
 }
